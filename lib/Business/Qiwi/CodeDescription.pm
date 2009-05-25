bc4b062df6b5b9e17289fc6272864e0e6b48fd8f
ksurent <ksurent@gmail.com>
2009-05-25 18:31:18 +0400

    removed due to redisign

--- lib/Business/Qiwi/CodeDescription.pm
+++ lib/Business/Qiwi/CodeDescription.pm
@@ -1,37 +1,37 @@
 use MooseX::Declare;
 
-require DB_File;
-
 class Business::Qiwi::CodeDescription extends Business::Qiwi::Request {
     has '+request_type' => ( default => 6, );
 
-    has cache => ( is => 'ro', isa => 'Str', default => '/tmp/', init_arg => undef, );
+    has cache_dir => ( is => 'ro', isa => Str, default => '/tmp/', );
+    has cached    => ( is => 'rw', isa => 'DBM::Deep', lazy => 1, init_arg => 1, );
 
-    augment create_request => sub {
-        my $self = shift;
-        
+    augment create_request() {
         return $self->_create_simple_node('request')
-    };
+    }
 
-    augment parse_raw_response => sub {
+    augment parse_raw_response() {
         my $self = shift;
         
         return {
             map { $_->getAttribute('id') => $_->data }
             $self->_xml_response->find('/response/response-codes/response-code')->get_nodelist
         }
-    };
+    }
 
-    after parse_raw_response => sub {
+    override result() {
+    }
+
+    after parse_raw_response() {
         my $self = shift;
         
         $self->_cache( $self->result )
-    };
+    }
     
-    method _cache(HashRef :$result) {
+    method _cache(HashRef $result) {
         
     }
-};
+}
 
 no Moose;
 no MooseX::Declare;
